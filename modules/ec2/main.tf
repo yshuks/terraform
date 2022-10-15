8952f84888bdb18f6691ee405070c2ee5047a0d4
yshuks <ysyash24@gmail.com>
2022-10-15 15:10:18 +0530

    commit

---
+++ modules/ec2/main.tf
@@ -0,0 +1,80 @@
+
+
+// Create aws_ami filter to pick up the ami available in your region
+data "aws_ami" "example" {
+  most_recent = true
+  owners      = ["137112412989"]
+
+  filter {
+    name   = "name"
+    values = ["al2022-ami-2022.0.20220728.1-kernel-5.15-x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+// Configure the EC2 instance in a public subnet
+resource "aws_instance" "ec2_public" {
+  ami                         = data.aws_ami.example.id
+  associate_public_ip_address = true
+  instance_type               = "t2.micro"
+  key_name                    = var.key_name
+  subnet_id                   = var.vpc.public_subnets[0]
+  vpc_security_group_ids      = [var.sg_pub_id]
+  user_data="${file("nginx.sh")}"
+
+  tags = {
+    "Name" = "EC2-PUBLIC"
+  }
+
+  # Copies the ssh key file to home dir
+  provisioner "file" {
+    source      = "./${var.key_name}.pem"
+    destination = "/home/ec2-user/${var.key_name}.pem"
+
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = file("${var.key_name}.pem")
+      host        = self.public_ip
+    }
+  }
+  
+  //chmod key 400 on EC2 instance
+  provisioner "remote-exec" {
+    inline = ["chmod 400 ~/${var.key_name}.pem"]
+
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = file("${var.key_name}.pem")
+      host        = self.public_ip
+    }
+
+  }
+
+}
+
+// Configure the EC2 instance in a private subnet
+resource "aws_instance" "ec2_private" {
+  ami                         = data.aws_ami.example.id
+  associate_public_ip_address = false
+  instance_type               = "t2.micro"
+  key_name                    = var.key_name
+  subnet_id                   = var.vpc.private_subnets[1]
+  vpc_security_group_ids      = [var.sg_priv_id]
+  user_data="${file("nginx.sh")}"
+
+  tags = {
+    "Name" = "EC2-PRIVATE"
+  }
+
+}
---
+++ nginx.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo yum update -y
+sudo yum install nginx -y
+sudo systemctl start nginx
+sudo systemctl enable nginx
+sudo systemctl status nginx
